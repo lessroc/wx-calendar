bad2c581de83f0e0f0d95a6104099b0820541ada
walker <hpf98@qq.com>
2025-05-16 01:07:55 +0800

    feat(dev): 更新日历组件并添加新功能
    - 引入并使用多个日历插件，包括农历、多选、禁用和ICS插件
    - 添加新的节日标记和禁用日期范围
    - 集成ICS日历数据加载
    - 调整日历组件的样式和布局
    - 更新项目配置，启用热重载功能

--- dev/pages/index/index.js
+++ dev/pages/index/index.js
@@ -5,37 +5,52 @@
  * @LastEditors: Please set LastEditors
  * @LastEditTime: 2024-11-25 22:22:23
  */
+const { WxCalendar } = require('@lspriv/wx-calendar/lib');
+const { LunarPlugin, LUNAR_PLUGIN_KEY } = require('@lspriv/wc-plugin-lunar');
+const { DisabledPlugin, DISABLED_PLUGIN_KEY } = require('@lspriv/wc-plugin-disabled');
+const { MultiPlugin, MULTI_PLUGIN_KEY } = require('@lspriv/wc-plugin-multiple');
+const { ICSPlugin, ICS_PLUGIN_KEY, ICSCnPreset } = require('@lspriv/wc-plugin-ics');
+
+WxCalendar.use(LunarPlugin);
+WxCalendar.use(MultiPlugin, {
+  type: 'multi',
+  bgColor: { light: '#409EFF', dark: '#409EFF' },
+  textColor: { light: '#FFF', dark: '#E5E5E5' }
+});
+WxCalendar.use(DisabledPlugin);
+// 使用ICSCnPreset预设
+WxCalendar.use(ICSPlugin, ICSCnPreset);
+
 Page({
   data: {
     padding: 0,
     markers: [
-      { year: 2024, month: 11, day: 19, type: 'festival', text: '国庆节', style: { color: '#409EFF' } },
-      {
-        year: 2024,
-        month: 11,
-        day: 19,
-        type: 'corner',
-        text: '休',
-        style: { color: '#409EFF' }
-      },
-      {
-        year: 2025,
-        month: 1,
-        day: 25,
-        type: 'schedule',
-        text: '呵呵',
-        style: { color: '#409EFF' }
-      }
+      { year: 2025, month: 5, day: 19, type: 'festival', text: '88', style: { color: '#f00' } },
+      { year: 2025, month: 5, day: 20, type: 'festival', text: '99', style: { color: '#0f0' } }
     ]
   },
   onLoad() {
-    const { bottom } = wx.getMenuButtonBoundingClientRect();
-    this.setData({
-      padding: bottom
-    });
+    // const { bottom } = wx.getMenuButtonBoundingClientRect();
+    // this.setData({
+    //   padding: bottom
+    // });
   },
   handleLoad() {
     const calendar = this.selectComponent('#calendar');
+
+    const disabledPlugin = calendar.getPlugin(DISABLED_PLUGIN_KEY);
+    disabledPlugin.disable([
+      // { year: 2025, month: 5, day: 7 }, // 禁用单个日期
+      // 禁用一个范围的日期
+      [
+        { year: -1900, month: 1, day: 1 },
+        { year: 2025, month: 5, day: 15 }
+      ]
+    ]);
+
+    const ics = calendar.getPlugin(ICS_PLUGIN_KEY);
+    ics.load('https://gitee.com/congqiu/ChineseHoliday/raw/master/holiday.ics');
+
     console.log('calendar-load', calendar);
   },
   handleClick({ detail }) {
